9818f69df97888857bdf37d215860c05bcbae195
unknown <ercheung3@697-334-Dale.sf.k12.internal>
2014-09-10 11:00:27 -0700

    Dice Program

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die bob,joe,billy,blah;
-void setup()
+public void setup()
 {
 	size(400,400);
 	background(100,100,100);
@@ -10,7 +26,7 @@ void setup()
 	// blah = new Dice(mouseX,mouseY);
 
 }
-void draw()
+public void draw()
 {
 	joe.roll();
 	joe.show();
@@ -29,7 +45,7 @@ void draw()
 	text(joe.dots+bob.dots, 310,195);
 	text("=",260,195);
 }
-void mousePressed()
+public void mousePressed()
 {
 	background(100,100,100);
 	redraw();
@@ -42,11 +58,11 @@ class Die //models one single dice cube
 		myX = x;
 		myY = y;
 	}
-	void roll()
+	public void roll()
 	{
 		dots=(int)(Math.random()*6)+1;
 	}
-	void show()
+	public void show()
 	{
 		fill(255,255,255);
 		rect(myX,myY,60,60,7);
@@ -95,4 +111,13 @@ class Die //models one single dice cube
 			ellipse(myX+15,myY+15,10,10);
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
